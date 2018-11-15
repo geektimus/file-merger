23bc5e41d95f4b361f51185411f191936986e00f
Geektimus <alexander.kno+github@gmail.com>
2018-11-15 16:37:48 +0100

    Base code to parse and print the workflow json file

--- main.go
+++ main.go
@@ -1,5 +1,52 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type HLTPTransformationWrapper struct {
+	Jars           []string `json:"jars"`
+	Schema         []string `json:"schema"`
+	Transformation []string `json:"transformation"`
+}
+
+type HLTPDescriptor struct {
+	Wrapper HLTPTransformationWrapper `json:"transform"`
+}
+
 func main() {
-	
+	// Open our jsonFile
+	jsonFile, err := os.Open("workflow.json")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened workflow.json")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	// bytes
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var descriptor HLTPDescriptor
+	err = json.Unmarshal([]byte(byteValue), &descriptor)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//
+	for _, e := range descriptor.Wrapper.Schema {
+		fmt.Println(e)
+	}
+
+	for _, e := range descriptor.Wrapper.Transformation {
+		fmt.Println(e)
+	}
+
 }
